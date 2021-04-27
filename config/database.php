e66ba9fab92634ff2acb77d432d70ea6fced4696
Ekene <ekennyobiasogu@gmail.com>
2021-04-27 11:01:51 +0100

    nav

--- config/database.php
+++ config/database.php
@@ -3,17 +3,17 @@
 class Database{
  
     //specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "zuridb";
-    private $username = "root";
-    private $password = "";
-    public $conn;
-
-    // private $host = "remotemysql.com";
-    // private $db_name = "qnW6GtII8g";
-    // private $username = "qnW6GtII8g";
-    // private $password = "ilztEyWXc6";
+    // private $host = "localhost";
+    // private $db_name = "zuridb";
+    // private $username = "root";
+    // private $password = "";
     // public $conn;
+
+    private $host = "remotemysql.com";
+    private $db_name = "qnW6GtII8g";
+    private $username = "qnW6GtII8g";
+    private $password = "ilztEyWXc6";
+    public $conn;
 
     // get the database connection
     public function getConnection(){
